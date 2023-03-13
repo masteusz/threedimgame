58a9a4f137141393f088ce2c78a38057b79fff54
Mateusz Stankiewicz <mateusz@mateusz.at>
2023-03-13 22:52:41 +0100

    Add level map and state.

--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,33 @@ unsafe fn update() {
         vline(80, 20, 120);
     }
 }
+
+const MAP: [u16; 8] = [
+    0b1111111111111111,
+    0b1000001010000101,
+    0b1011100000110101,
+    0b1000111010010001,
+    0b1010001011110111,
+    0b1011101001100001,
+    0b1000100000001101,
+    0b1111111111111111,
+];
+
+fn point_in_wall(x: f32, y: f32) -> bool {
+    match MAP.get(y as usize) {
+        Some(line) => (line & (0b1 << x as usize)) != 0,
+        None => true,
+    }
+}
+
+struct State {
+    player_x: f32,
+    player_y: f32,
+    player_angle: f32,
+}
+
+static mut STATE: State = State {
+    player_x: 1.5,
+    player_y: 1.5,
+    player_angle: 0.0,
+};
