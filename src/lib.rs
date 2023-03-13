30bcbf705b8cb34c4c61fcd142586e264361a3b4
Mateusz Stankiewicz <mateusz@mateusz.at>
2023-03-13 22:41:39 +0100

    Removed Cargo.lock. Some updates

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+
+use core::{arch::wasm32, panic::PanicInfo};
+
+const GAMEPAD1: *const u8 = 0x16 as *const u8;
+
+// 00010000
+const BUTTON_LEFT: u8 = 16;
+
+// 00100000
+const BUTTON_RIGHT: u8 = 32;
+
+// 01000000
+const BUTTON_UP: u8 = 64;
+
+// 10000000
+const BUTTON_DOWN: u8 = 128;
+
+extern "C" {
+    fn vline(x: i32, y: i32, len: u32);
+}
+
+#[panic_handler]
+fn phandler(_: &PanicInfo<'_>) -> ! {
+    wasm32::unreachable();
+}
+
+#[no_mangle]
+unsafe fn update() {
+    if *GAMEPAD1 & BUTTON_UP != 0 {
+        vline(80, 20, 120);
+    }
+}
